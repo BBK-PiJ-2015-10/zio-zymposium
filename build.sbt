864cf227647b77889e1101bc5df63ef49a7412dc
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2022-09-19 09:26:05 -0400

    kicking off example

--- build.sbt
+++ build.sbt
@@ -3,12 +3,16 @@ ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "com.example"
 ThisBuild / organizationName := "example"
 
+lazy val zioVersion = "2.0.2"
+
 lazy val root = (project in file("."))
   .settings(
     name := "zio-zymposium",
     libraryDependencies ++= Seq(
-      "dev.zio" %% "zio" % "2.0.2",
-      "dev.zio" %% "zio-test" % "2.0.2" % Test
+      "dev.zio" %% "zio" % zioVersion,
+      "dev.zio" %% "zio-streams" % zioVersion,
+
+      "dev.zio" %% "zio-test" % zioVersion % Test
     ),
     testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework")
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,23 @@
 
 import zio._
+import zio.stream._
+
+import java.io.IOException
 //import zio.Console.printLine
+
+//Concept       ZIO 1     vs   ZIO 2
+//Beginning    ZStream        ZStream
+//Middle       Transducer     ZPipeline
+//End          ZSink          ZSink
 
 object Main extends ZIOAppDefault {
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    ZIO.succeed("alexis") *> Console.printLine("ale")
+  val stream: ZStream[Any, IOException, Unit] =
+    ZStream.repeatZIO(Console.printLine("Hello, Zymposium"))
+      .take(10)
 
+  val testRunner: IO[IOException, Unit] = Console.printLine("alexis")
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    stream.runDrain.debug("RESULT")
 }
