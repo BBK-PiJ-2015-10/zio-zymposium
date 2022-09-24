6eb60b0b046609b2e4290d0f917ac5f5258f7f17
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2022-09-24 17:02:29 -0400

    adding fan-in examples

--- src/main/scala/zstreams/FanInFanOutExamples.scala
+++ src/main/scala/zstreams/FanInFanOutExamples.scala
@@ -44,6 +44,7 @@ object FanInFanOutExamples extends ZIOAppDefault{
     final case class NotificationEvent(description: String) extends Event
   }
 
+
   val eventStream: ZStream[Any, Nothing, Event] =
     ZStream(SecurityEvent(9000),NotificationEvent("hello"),SecurityEvent(3000),NotificationEvent("ciao"))
 
@@ -67,8 +68,81 @@ object FanInFanOutExamples extends ZIOAppDefault{
           processNotificationEvent(notificationEvents).runDrain
     }
 
+  /*           stream2
+      stream1             stream4
+               stream3
+   */
+  val runFanInAFanOut =
+    partitions.flatMap {
+      case (securityEvents,notificationEvents) =>
+        val processedSecurityEvents = processSecurityEvent(securityEvents).map(Left(_))
+        val processedNotificationEvents = processNotificationEvent(notificationEvents).map(Right(_))
+        val merged: ZStream[Any, Nothing, Either[Int, String]] = processedSecurityEvents.merge(processedNotificationEvents)
+        merged.tap{
+          ale => ZIO.logInfo(s"CULON $ale")
+        }.runDrain
+    }
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = partitionsEamples
+  val runFanInAFanOutEx2: ZStream[Any, Nothing, Either[Int, String]] = {
+    ZStream.unwrapScoped(
+    partitions.map {
+    case (securityEvents, notificationEvents) =>
+      val processedSecurityEvents = processSecurityEvent(securityEvents).map(Left(_))
+      val processedNotificationEvents = processNotificationEvent(notificationEvents).map(Right(_))
+      processedSecurityEvents.merge(processedNotificationEvents)
+  })
+  }
+
+  /*
+    Variations of fan-in:
+    merge - pull from both upstreams as quickly as they have stuff and emit unchanged
+     RED,          BLUE
+         RED,GREEN
+    merge
+    RED,RED,GREEN,BLUE
+    zip / zipWith - pairwise combine them, pull from both upstreams and emit one value once you have a value from both
+    [RED,RED][BLUE,GREEN]
+    zipWithLatest - pairwise combine them, hold on to the last value seen from each side, emit new value
+    (RED,RED) (RED,GREEN) (BLUE,GREEN)
+   */
+
+  sealed trait Marble
+
+  object Marble {
+    case object Red extends Marble
+    case object Green extends Marble
+    case object Blue extends Marble
+  }
+
+  def delayedStream[A](values: (A,Duration)*): ZStream[Any,Nothing,A] = {
+    values.toList match {
+      case (value,duration) :: rest =>
+      ZStream.fromZIO(ZIO.succeed(value).delay(duration)) ++ delayedStream(rest: _*)
+      case _ => ZStream.empty
+    }
+  }
+
+  val stream1Marbles = delayedStream(Marble.Red -> 1.second, Marble.Blue -> 3.seconds)
+  val stream2Marbles = delayedStream(Marble.Red -> 2.seconds, Marble.Green -> 1.seconds)
+
+  val mergeMarbleExample =
+    stream1Marbles.merge(stream2Marbles).debug.runDrain
+
+  val zipMarbleExample =
+    stream1Marbles.zip(stream2Marbles).debug.runDrain
+
+  val zipWithLatestMarbleExample =
+    stream1Marbles.zipWithLatest(stream2Marbles)((_,_)).debug.runDrain
+
+  /*
+      zip / zipWith - pairwise combine them, pull from both upstreams and emit one value once you have a value from both
+
+   */
+
+
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = zipWithLatestMarbleExample
+    //runFanInAFanOutEx2.debug("Merged").runCollect.debug
 
   //left on 24:36 https://www.youtube.com/watch?v=3EO0yVf63xI
 
