dbc39bb37b4e696beeecd4b3a9c69d114e333b15
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2022-09-25 17:16:53 -0400

    Using hubs, references, and promises

--- src/main/scala/zhubs/Zhubs.scala
+++ src/main/scala/zhubs/Zhubs.scala
@@ -67,18 +67,17 @@ object Zhubs extends ZIOAppDefault {
       hub.offer(i) *> ZIO.sleep(100.milliseconds)
     }
 
-  def naiveHubConsumer(id: Int)(hub: NaiveHub[Int]): ZIO[Any, Nothing, Nothing] =
+  def naiveHubConsumer(label: String,id: Int)(hub: NaiveHub[Int]): ZIO[Any, Nothing, Nothing] =
     hub.take(id).flatMap { i =>
-      Console.printLine(s"Consumer with id: $id got $i").!
+      Console.printLine(s"Consumer with label: $label got $i").!
     }.forever
-
 
 
   val queueExampleWithNaiveHub = for {
     hub <- NaiveHub.make[Int](2)
     producerFib <- naiveHubProducer(hub).fork
-    consumerFib1 <- naiveHubConsumer(0)(hub).fork
-    consumerFib2 <- naiveHubConsumer(1)(hub).fork
+    consumerFib1 <- naiveHubConsumer("logging framework",0)(hub).fork
+    consumerFib2 <- naiveHubConsumer("database",1)(hub).fork
     _ <- producerFib.join
     _ <- ZIO.sleep(1.seconds)
     _ <- consumerFib1.interrupt
@@ -87,9 +86,70 @@ object Zhubs extends ZIOAppDefault {
 
 
 
+  def hubProducer(hub: Hub[Int]): ZIO[Any, Nothing, Unit] =
+    ZIO.foreachDiscard(0 to 5) { i =>
+      hub.publish(i) *> ZIO.sleep(100.milliseconds)
+    }
+
+  def hubConsumer(label: String)(hub: Hub[Int]) = {
+    val subscription = for {
+      deQueue <- hub.subscribe
+      article <- deQueue.take
+      _       <- Console.printLine(s"$label got $article").!
+    } yield ()
+    subscription.forever
+  }
 
 
+  lazy val queueExampleWithHub = for {
+    hub <- Hub.bounded[Int](16)
+    _ <- hubConsumer("logging framework")(hub).fork
+    _ <- hubConsumer("database")(hub).fork
+    _ <- hubProducer(hub).fork
+    _ <- ZIO.sleep(1.seconds)
+  } yield ()
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = queueExampleWithNaiveHub
+  def hubConsumerWithCounter(label: String)(hub: Hub[Int])(currentSubscribers: Ref[Int],start: Promise[Nothing,Unit]) = {
+    val subscription = for {
+      deQueue <- hub.subscribe
+      n       <- currentSubscribers.updateAndGet(_ + 1)
+      _       <-  if (n == 4) start.succeed(()) else ZIO.unit
+      article <- deQueue.take
+      _       <- Console.printLine(s"$label got $article").!
+    } yield ()
+    subscription.forever
+  }
+
+  lazy val queueExampleWithHubWithCounter = for {
+    hub <- Hub.bounded[Int](16)
+    ref <- Ref.make(0)
+    promise <- Promise.make[Nothing,Unit]
+    _ <- hubConsumerWithCounter("logging framework")(hub)(ref,promise).fork
+    _ <- hubConsumerWithCounter("database")(hub)(ref,promise).fork
+    _ <- hubConsumerWithCounter("data eager")(hub)(ref,promise).fork
+    _ <- hubConsumerWithCounter("info destroyer")(hub)(ref,promise).fork
+    _ <- promise.await
+    _ <- hubProducer(hub).fork
+    _ <- ZIO.sleep(1.seconds)
+  } yield ()
+
+
+  /*
+     Naive Hub
+     Queue1   | |2|3|4|5|
+     Queue2   | | |3|4|5|
+   */
+
+  /*
+     ZIO Hub
+     Array | |2|3|4|5|
+     consumer1   index == 1
+     consumer2   index == 2
+   */
+
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = queueExampleWithHubWithCounter
+
+  //left on minute  40:17
 
 }
